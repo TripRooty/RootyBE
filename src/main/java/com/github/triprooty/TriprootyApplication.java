f98562032b48f58490fe7c95366f680908d3b29a
nenini <kyr068600@gmail.com>
2025-10-04 00:52:03 +0900

    chore : 이슈 템플릿 및 PR 템플릿 세팅

--- src/main/java/com/github/triprooty/TriprootyApplication.java
+++ src/main/java/com/github/triprooty/TriprootyApplication.java
@@ -2,8 +2,10 @@ package com.github.triprooty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TriprootyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/triprooty/demo/TestEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.triprooty.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-}
