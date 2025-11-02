92d29d0a35fbb4669115ba34579685562c571edb
LeeMin-hyeong <lmhstrar070@naver.com>
2025-11-02 20:01:35 +0900

    fix : remove unused return
    Signed-off-by: LeeMin-hyeong <lmhstrar070@naver.com>

--- src/main/java/com/github/triprooty/dto/response/auth/EmailCodeVerifyResponse.java
+++ src/main/java/com/github/triprooty/dto/response/auth/EmailCodeVerifyResponse.java
@@ -1,12 +1,3 @@
 package com.github.triprooty.dto.response.auth;
 
-import java.time.Duration;
-
-public record EmailCodeVerifyResponse(Boolean success, String emailVerifiedToken) {
-    public EmailCodeVerifyResponse(Boolean success) {
-        this(success, "");
-    }
-    public EmailCodeVerifyResponse(String emailVerifiedToken) {
-        this(true, emailVerifiedToken);
-    }
-}
+public record EmailCodeVerifyResponse(String emailVerifiedToken) { }
